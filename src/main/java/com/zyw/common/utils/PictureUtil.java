bc4b99df5c7ed54de0dcba61b437b1268a25367b
minggen <919767736@qq.com>
2019-11-25 10:06:59 +0800

    Update PictureUtil.java

--- src/main/java/com/zyw/common/utils/PictureUtil.java
+++ src/main/java/com/zyw/common/utils/PictureUtil.java
@@ -4,10 +4,16 @@ package com.zyw.common.utils;
 import javax.swing.*;
 
 public class PictureUtil {
-    private static String basePath = PictureUtil.class.getResource("/imgs/").getPath();
-
     public static ImageIcon getPicture(String name) {
-        ImageIcon icon = new ImageIcon(basePath + name);
-        return icon;
+        ImageIcon imageIcon = new ImageIcon();
+        try {
+            InputStream resourceAsStream = PictureUtil.class.getClassLoader().getResourceAsStream("imgs/" + name);
+            if (resourceAsStream != null) {
+                imageIcon = new ImageIcon(ImageIO.read(resourceAsStream));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return imageIcon;
     }
 }
